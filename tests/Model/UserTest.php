3eb2d6823012f85f346122bb48eb42d81a38fb52
Stephen Hill <stephen@gatekiller.co.uk>
2015-02-02 15:35:13 +0000

    Added unit tests for the Wiki\Model\User class.

---
+++ tests/Model/UserTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use Wiki\Model\User;
+
+class UserTest extends PHPUnit_Framework_TestCase
+{
+    public function testConstructor()
+    {
+        $user = new User();
+
+        $this->assertInstanceOf(User::Class, $user);
+    }
+
+    public function testProperties()
+    {
+        $user = new User();
+        $user->setEmail('wiki@example.com');
+
+        $this->assertSame('wiki@example.com', $user->getEmail());
+    }
+    
+    public function testPassword()
+    {
+        $user = new User();
+        $user->setPassword('qUI&Jjt^I5J&p0Kk');
+
+        $this->assertTrue($user->isPasswordMatch('qUI&Jjt^I5J&p0Kk'));
+    }
+}
