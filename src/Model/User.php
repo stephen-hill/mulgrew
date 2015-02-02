184dc94047c75d4ef77801a6c1843d6d28b83a0f
Stephen Hill <stephen@gatekiller.co.uk>
2015-02-02 13:29:58 +0000

    Added setPassword(), isPasswordMatch() and hash() methods, for working with passwords.

--- src/Model/User.php
+++ src/Model/User.php
@@ -20,5 +20,29 @@ class User
      * User's hashed password
      * @var string
      */
-    protected $password;
+    private $password;
+
+    /**
+     * Sets the User's hashed password.
+     *
+     * @param string $password the password
+     *
+     * @return self
+     */
+    public function setPassword($password)
+    {
+        $this->password = $this->hash($password);
+
+        return $this;
+    }
+
+    public function isPasswordMatch($password)
+    {
+        return $this->hash($password) === $this->password;
+    }
+    
+    protected function hash($password)
+    {
+        return hash_pbkdf2('sha256', $password, $this->email, 256E3, 0, true);
+    }
 }
