8dc409bc6b3edfa6d1000f445d13ad8a69ee765b
Stephen Hill <stephen@gatekiller.co.uk>
2015-03-24 07:33:37 +0000

    Added methods.

--- src/Model/Note.php
+++ src/Model/Note.php
@@ -7,27 +7,89 @@ use Doctrine\Common\Collections\Collection;
 class Note
 {
     /**
-     * Identifier
+     * Identifier.
+     *
      * @var integer
      */
     protected $id;
 
     /**
-     * Case-sensetive Title
+     * Case-sensetive Title.
+     *
      * @var string
      */
     protected $title;
-    
+
     /**
-     * The current text of the page
+     * The current text of the page.
+     *
      * @var string
      */
     protected $text;
 
     /**
-     * Collection of all revisions
+     * Collection of all revisions.
+     *
      * @var Collection
      * @OneToMany(targetEntity="Mulgrew\Model\Revision", mappedBy="note")
      */
     protected $revisions;
+
+    /**
+     * Gets the Case-sensetive Title.
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Sets the Case-sensetive Title.
+     *
+     * @param string $title the title
+     *
+     * @return self
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Gets the The current text of the page.
+     *
+     * @return string
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * Sets the The current text of the page.
+     *
+     * @param string $text the text
+     *
+     * @return self
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    /**
+     * Gets the Identifier.
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
 }
