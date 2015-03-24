ddc1e589fb298ffeea58438d329004955d67f929
Stephen Hill <stephen@gatekiller.co.uk>
2015-03-24 07:34:05 +0000

    Added some whitespace to docs.

--- src/Model/Revision.php
+++ src/Model/Revision.php
@@ -7,39 +7,46 @@ use DateTimeInterface;
 class Revision
 {
     /**
-     * Revision Identifier
+     * Revision Identifier.
+     *
      * @column(type="string", length="40", )
+     *
      * @var integer
      */
     protected $id;
 
     /**
-     * The page
+     * The page.
+     *
      * @var Mulgrew\Model\Page
      * @ManyToOne(targetEntity="Mulgrew\Model\Page", inversedBy="revisions")
      */
     protected $page;
 
     /**
-     * The user who created the revision
+     * The user who created the revision.
+     *
      * @var Mulgrew\Model\User
      */
     protected $user;
 
     /**
-     * Opcodes to get to the previous revision
+     * Opcodes to get to the previous revision.
+     *
      * @var string|null
      */
     protected $opcodes;
 
     /**
-     * Optional comment for this revision
+     * Optional comment for this revision.
+     *
      * @var string
      */
     protected $comment;
 
     /**
      * The date and time the revision was created.
+     *
      * @var DateTimeInterface
      */
     protected $datetime;
