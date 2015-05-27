6cf375bda370c46dd0c762e65d11c1b5878eb86c
Stephen Hill <stephen@gatekiller.co.uk>
2015-05-27 16:49:21 +0100

    Test that the notes collection works.

--- tests/Model/NotebookTest.php
+++ tests/Model/NotebookTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Mulgrew\Model\Note;
 use Mulgrew\Model\Notebook;
 
 class NotebookTest extends PHPUnit_Framework_TestCase
@@ -11,11 +12,26 @@ class NotebookTest extends PHPUnit_Framework_TestCase
         $this->assertInstanceOf(Notebook::Class, $notebook);
     }
 
-    public function testProperties()
+    public function testTitle()
     {
         $notebook = new Notebook();
         $notebook->setTitle('Example Notebook');
 
         $this->assertSame('Example Notebook', $notebook->getTitle());
+    }
+
+    public function testNotes()
+    {
+        $notebook = new Notebook();
+        $noteA = new Note();
+        $noteB = new Note();
+
+        $noteA->setTitle('Note A');
+        $noteB->setTitle('Note B');
+
+        $notebook->getNotes()->add($noteA);
+        $notebook->getNotes()->add($noteB);
+
+        $this->assertSame(2, $notebook->getNotes()->count());
     }
 }
